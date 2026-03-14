1e4a0a0d84b28a797d3f3ad96b7776b5df061cc7
Osama <osama.yousef@tuta.io>
2026-03-14 18:01:51 +0200

    feat: stripe whitespaces by default & a custom flag

--- src/main.rs
+++ src/main.rs
@@ -5,61 +5,70 @@ struct Config {
     files: Vec<String>,
     case_sensitivity: bool,
     line_numbers: bool,
+    stripe_ws: bool,
 }
 
 impl Config {
     fn new(args: &Vec<String>) -> Result<Config, String> {
         if args.len() < 3 {
-            return Err(format!("Usage: {} PATTERN FILE... [-i] [-n]", args[0]));
+            return Err(format!("Usage: {} PATTERN FILE... [-i] [-n] [-S]", args[0]));
         }
 
         let mut case_sensitivity = false;
         let mut line_numbers = false;
+        let mut stripe_ws = true;
         let mut non_flagged = Vec::new();
 
         for arg in &args[1..] {
-            if arg == "-i" {
-                case_sensitivity = true;
-            } else if arg == "-n" {
-                line_numbers = true;
-            } else {
-                non_flagged.push(arg.clone());
+            match arg.as_str() {
+                "-i" => case_sensitivity = true,
+                "-n" => line_numbers = true,
+                "-S" => stripe_ws = false,
+                _ => non_flagged.push(arg.clone())
             }
         }
 
-        let pattern = non_flagged[0].clone();
-        let files = non_flagged[1..].to_vec();
+        if non_flagged.len() < 2 {
+            return Err(String::from("Error: Missing PATTERN or FILE arguments."));
+        }
 
-        Ok(Config { pattern, files, case_sensitivity, line_numbers })
+        let pattern = non_flagged.remove(0);
+        let files = non_flagged.to_vec();
+
+        Ok(Config { pattern, files, case_sensitivity, line_numbers, stripe_ws })
 
     }
 }
 
-fn grep(reader: &mut BufReader<File>, pattern: &str, case_sensitivity: bool, line_number: bool) {
+fn grep(reader: &mut BufReader<File>, pattern: &str, case_sensitivity: bool, line_number: bool, stripe_ws: bool) {
     let mut line = String::new();
     let mut i = 0;
 
     while let Ok(bytes_read) = reader.read_line(&mut line) {
-        if bytes_read == 0 {
-            break;
-        }
-
-        let matched;
-        if case_sensitivity {
-            matched = line.to_lowercase().contains(&pattern.to_lowercase());
-        } else {
-            matched = line.contains(&pattern);
-        }
-
+        if bytes_read == 0 { break; }
         i += 1;
+
+        let matched = if case_sensitivity {
+            line.contains(pattern)
+        } else {
+            line.to_lowercase().contains(&pattern.to_lowercase())
+        };
+
+        if !matched {
+            line.clear();
+            continue;
+        }
+
+        if stripe_ws {
+            line = line.trim().to_string();
+        }
+
         if line_number {
             let new_prefix = format!("{}: ", i);
             line.insert_str(0, &new_prefix);
         }
 
-        if matched {
-            println!("{}", line);
-        }
+        println!("{}", line);
         line.clear();
     }
 }
@@ -68,7 +77,7 @@ fn grep_file(file: &str, config: &Config) -> () {
     match File::open(file) {
         Ok(f) => {
             let mut reader = BufReader::new(f);
-            grep(&mut reader, &config.pattern, config.case_sensitivity, config.line_numbers);
+            grep(&mut reader, &config.pattern, config.case_sensitivity, config.line_numbers, config.stripe_ws);
         }
         Err(e) => {
             eprintln!("Could not open a file: '{}', {}", file, e);
